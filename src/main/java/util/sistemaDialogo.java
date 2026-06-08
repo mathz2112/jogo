3ed5644948921e2273a0a9eec5aaac034bd71ef7
mathz2112 <matheus.araujo7@estudante.ifms.edu.br>
2026-06-08 08:04:06 -0400

    mudanca em sistemaDialogo

--- src/main/java/util/sistemaDialogo.java
+++ src/main/java/util/sistemaDialogo.java
@@ -5,7 +5,10 @@
 
 package util;
 
+import util.Local;
+
 public class sistemaDialogo {
+
     private Local localAtual;
     private int dialogoAtual;
 
@@ -31,5 +34,79 @@ public class sistemaDialogo {
         return dialogoAtual;
     }
 
+    public String getNomeAtual() {
+        return "Voce";
+    }
+
+    public String getTextoAtual() {
+
+        switch(localAtual) {
+
+            case ENTRADA:
+
+                switch(dialogoAtual) {
+
+                    case 0:
+                        return "Dialogo de teste 1";
+
+                    case 1:
+                        return "Dialogo de teste 2";
+
+                    case 2:
+                        return "Para onde devo ir?";
+                }
+
+                break;
+
+            case BLOCO_A:
+                return "Dialogo temporario do Bloco A.";
+
+            case BLOCO_B:
+                return "Dialogo temporario do Bloco B.";
+
+            case BLOCO_C:
+                return "Dialogo temporario do Bloco C.";
+
+            case BLOCO_D_TERREO:
+                return "Dialogo temporario do Bloco D Terreo.";
+
+            case BLOCO_D_SEGUNDO_ANDAR:
+                return "Dialogo temporario do Bloco D Segundo Andar.";
+
+            case CAMINHO_BLOCO_E:
+                return "Dialogo temporario do Caminho para o Bloco E.";
+
+            case BLOCO_E:
+                return "Dialogo temporario do Bloco E.";
+
+            case BLOCO_F:
+                return "Dialogo temporario do Bloco F.";
+
+            case QUADRA:
+                return "Dialogo temporario da Quadra.";
+
+            case FLORESTA_AMORAS:
+                return "Dialogo temporario da Floresta de Amoras.";
+        }
+
+        return "";
+    }
+
+    public String getOpcao1() {
+
+        switch(localAtual) {
+
+            case ENTRADA:
+
+                if(dialogoAtual < 2) {
+                    return "Continuar";
+                }
+
+                return "Ir para o Bloco A";
+
+            default:
+                return "Continuar";
+        }
+    }
 }
 
