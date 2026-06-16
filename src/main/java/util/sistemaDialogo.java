866cef3ea1ca82d8e764b2ae78de86d4ad8397b1
mathz2112 <matheus.araujo7@estudante.ifms.edu.br>
2026-06-16 08:08:56 -0400

    atualização de sistemaDialogo

--- src/main/java/util/sistemaDialogo.java
+++ src/main/java/util/sistemaDialogo.java
@@ -13,14 +13,23 @@ public class sistemaDialogo {
 
     private Local localAtual;
     private int dialogoAtual;
+    private boolean introducaoConcluida;
 
     public sistemaDialogo() {
         localAtual = Local.ENTRADA;
         dialogoAtual = 0;
+        introducaoConcluida = false;
     }
 
     public void proximoDialogo() {
+
         dialogoAtual++;
+
+        if(localAtual == Local.ENTRADA &&
+           dialogoAtual >= getDialogos().size()) {
+
+            introducaoConcluida = true;
+        }
     }
 
     public void mudarLocal(Local local) {
@@ -42,7 +51,7 @@ public class sistemaDialogo {
 
     public String getNomeAtual() {
 
-        if (terminouDialogo()) {
+        if(terminouDialogo()) {
             return "";
         }
 
@@ -51,7 +60,7 @@ public class sistemaDialogo {
 
     public String getTextoAtual() {
 
-        if (terminouDialogo()) {
+        if(terminouDialogo()) {
             return "Escolha para onde deseja ir.";
         }
 
@@ -62,179 +71,267 @@ public class sistemaDialogo {
 
         List<Fala> falas = new ArrayList<>();
 
-        switch (localAtual) {
+        switch(localAtual) {
 
             case ENTRADA:
 
-                falas.add(new Fala("Você",
-                        "Já faz semanas que a Pintas desapareceu, e ninguém fez nada."));
+                if(introducaoConcluida) {
+                    break;
+                }
 
-                falas.add(new Fala("Você",
-                        "Talvez, procurando pelo campus, eu consiga encontrar alguma pista."));
+                falas.add(new Fala(
+                        "Você",
+                        "Já faz semanas que a Pintas desapareceu, e ninguém fez nada."
+                ));
 
-                falas.add(new Fala("Você",
-                        "Eu sei que não é a melhor ideia entrar na escola tão tarde, mas o que de pior pode acontecer, né?"));
+                falas.add(new Fala(
+                        "Você",
+                        "Talvez, procurando pelo campus, eu consiga encontrar alguma pista."
+                ));
+
+                falas.add(new Fala(
+                        "Você",
+                        "Eu sei que não é a melhor ideia entrar na escola tão tarde, mas o que de pior pode acontecer, né?"
+                ));
 
                 break;
 
             case BLOCO_A:
 
-                falas.add(new Fala("Você",
-                        "Um livro do governo? Talvez eu possa usar isso para me defender."));
+                falas.add(new Fala(
+                        "Você",
+                        "Um livro do governo? Talvez eu possa usar isso para me defender."
+                ));
 
-                falas.add(new Fala("Sistema",
-                        "Você encontrou o Livro do Governo."));
+                falas.add(new Fala(
+                        "",
+                        "Você encontrou o Livro do Governo."
+                ));
 
                 break;
 
             case BLOCO_B:
 
-                falas.add(new Fala("Você",
-                        "Está trancado."));
+                falas.add(new Fala(
+                        "Você",
+                        "Está trancado."
+                ));
 
                 break;
 
             case BLOCO_C:
 
-                falas.add(new Fala("Sistema",
-                        "Você se depara com um quero-quero raivoso. Você tenta se afastar, mas ele é mais rápido."));
+                falas.add(new Fala(
+                        "",
+                        "Você se depara com um quero-quero raivoso. Você tenta se afastar, mas ele é mais rápido."
+                ));
 
-                falas.add(new Fala("Sistema",
-                        "[INICIAR BATALHA]"));
+                falas.add(new Fala(
+                        "",
+                        "[INICIAR BATALHA]"
+                ));
 
-                falas.add(new Fala("Sistema",
-                        "Você ganhou +50 de XP."));
+                falas.add(new Fala(
+                        "",
+                        "Você ganhou +50 de XP."
+                ));
 
-                falas.add(new Fala("Você",
-                        "O que foi isso? Que bloco esquisito..."));
+                falas.add(new Fala(
+                        "Você",
+                        "O que foi isso? Que bloco esquisito..."
+                ));
 
                 break;
 
             case BLOCO_D_HALL:
 
-                falas.add(new Fala("Você",
-                        "Para onde eu devo ir?"));
+                falas.add(new Fala(
+                        "Você",
+                        "Para onde eu devo ir?"
+                ));
 
                 break;
 
             case BLOCO_D_SEGUNDO_ANDAR:
 
-                falas.add(new Fala("Sistema",
-                        "Você encontrou um extintor de incêndio."));
+                falas.add(new Fala(
+                        "",
+                        "Você encontrou um extintor de incêndio."
+                ));
 
                 break;
 
             case CANTINA:
 
-                falas.add(new Fala("Você",
-                        "Aquelas são as gêmeas Lo e Ja? O que elas estão fazendo aqui a essa hora?"));
+                falas.add(new Fala(
+                        "Você",
+                        "Aquelas são as gêmeas Lo e Ja? O que elas estão fazendo aqui a essa hora?"
+                ));
 
-                falas.add(new Fala("Lo e Ja",
-                        "Ei! Gostaria de comprar um brownie? Custa 20 reais."));
+                falas.add(new Fala(
+                        "Lo e Ja",
+                        "Ei! Gostaria de comprar um brownie? Custa 20 reais."
+                ));
 
-                falas.add(new Fala("Você",
-                        "Mas o que vocês estão fazendo aqui?"));
+                falas.add(new Fala(
+                        "Você",
+                        "Mas o que vocês estão fazendo aqui?"
+                ));
 
-                falas.add(new Fala("Lo e Ja",
-                        "Vendendo brownies."));
+                falas.add(new Fala(
+                        "Lo e Ja",
+                        "Vendendo brownies."
+                ));
 
-                falas.add(new Fala("Você",
-                        "Mas por quê?"));
+                falas.add(new Fala(
+                        "Você",
+                        "Mas por quê?"
+                ));
 
-                falas.add(new Fala("Lo e Ja",
-                        "E por que não?"));
+                falas.add(new Fala(
+                        "Lo e Ja",
+                        "E por que não?"
+                ));
 
-                falas.add(new Fala("Sistema",
-                        "Você comprou um brownie. Coma-o para recuperar +20 de vida."));
+                falas.add(new Fala(
+                        "",
+                        "Você comprou um brownie. Coma-o para recuperar +20 de vida."
+                ));
 
                 break;
 
             case QUADRA:
 
-                falas.add(new Fala("Sistema",
-                        "Você encontrou a rede de vôlei. Ela pode ser usada uma única vez para reduzir o dano do adversário."));
+                falas.add(new Fala(
+                        "",
+                        "Você encontrou a rede de vôlei. Ela pode ser usada uma única vez para reduzir o dano do adversário."
+                ));
 
                 break;
 
             case CAMINHO_BLOCO_E:
 
-                falas.add(new Fala("Sistema",
-                        "Você se depara com uma coruja assustadora. Você tenta se afastar, mas ela é mais rápida."));
+                falas.add(new Fala(
+                        "",
+                        "Você se depara com uma coruja assustadora. Você tenta se afastar, mas ela é mais rápida."
+                ));
 
-                falas.add(new Fala("Sistema",
-                        "[INICIAR BATALHA]"));
+                falas.add(new Fala(
+                        "",
+                        "[INICIAR BATALHA]"
+                ));
 
-                falas.add(new Fala("Sistema",
-                        "Você ganhou +50 de XP."));
+                falas.add(new Fala(
+                        "",
+                        "Você ganhou +50 de XP."
+                ));
 
-                falas.add(new Fala("Você",
-                        "O que foi isso? Que bloco esquisito..."));
+                falas.add(new Fala(
+                        "Você",
+                        "O que foi isso? Que bloco esquisito..."
+                ));
 
                 break;
 
             case BLOCO_E:
 
-                falas.add(new Fala("Sistema",
-                        "O bloco está mergulhado na escuridão. De uma das janelas vem a única fonte de luz, revelando ao longe um sombrio bosque de amoras."));
+                falas.add(new Fala(
+                        "",
+                        "O bloco está mergulhado na escuridão. De uma das janelas vem a única fonte de luz, revelando ao longe um sombrio bosque de amoras."
+                ));
 
                 break;
 
             case BLOCO_F:
 
-                falas.add(new Fala("Sistema",
-                        "Logo à frente está o Tio do Pastel, do lado de fora das grades do IF."));
+                falas.add(new Fala(
+                        "",
+                        "Logo à frente está o Tio do Pastel, do lado de fora das grades do IF."
+                ));
 
-                falas.add(new Fala("Você",
-                        "Por que todo mundo está vendendo coisas a essa hora?"));
+                falas.add(new Fala(
+                        "Você",
+                        "Por que todo mundo está vendendo coisas a essa hora?"
+                ));
 
-                falas.add(new Fala("Tio do Pastel",
-                        "Hoje só temos pastel de carne. Vinte reais."));
+                falas.add(new Fala(
+                        "Tio do Pastel",
+                        "Hoje só temos pastel de carne. Vinte reais."
+                ));
 
-                falas.add(new Fala("Sistema",
-                        "Vida totalmente recuperada."));
+                falas.add(new Fala(
+                        "",
+                        "Vida totalmente recuperada."
+                ));
 
                 break;
 
             case FLORESTA_AMORAS:
 
-                falas.add(new Fala("Você",
-                        "Que lugar escuro... O quê? Aquela é... a Pintas?"));
+                falas.add(new Fala(
+                        "Você",
+                        "Que lugar escuro... O quê? Aquela é... a Pintas?"
+                ));
 
-                falas.add(new Fala("Pintas",
-                        "Ai, que bom que você me encontrou! Mas fala baixo, ou ele pode nos ouvir."));
+                falas.add(new Fala(
+                        "Pintas",
+                        "Ai, que bom que você me encontrou! Mas fala baixo, ou ele pode nos ouvir."
+                ));
 
-                falas.add(new Fala("Você",
-                        "Ele quem?"));
+                falas.add(new Fala(
+                        "Você",
+                        "Ele quem?"
+                ));
 
-                falas.add(new Fala("Capi",
-                        "Eu!"));
+                falas.add(new Fala(
+                        "Capi",
+                        "Eu!"
+                ));
 
-                falas.add(new Fala("Você",
-                        "É aquela capivara da UF?"));
+                falas.add(new Fala(
+                        "Você",
+                        "É aquela capivara da UF?"
+                ));
 
-                falas.add(new Fala("Capi",
-                        "Eu não sou aquela capivara da UF. Eu sou a maior mascote de Campo Grande e não vou deixar que essa oncinha roube meu lugar."));
+                falas.add(new Fala(
+                        "Capi",
+                        "Eu não sou aquela capivara da UF. Eu sou a maior mascote de Campo Grande e não vou deixar que essa oncinha roube meu lugar."
+                ));
 
-                falas.add(new Fala("Sistema",
-                        "[BATALHA FINAL]"));
+                falas.add(new Fala(
+                        "",
+                        "[BATALHA FINAL]"
+                ));
 
-                falas.add(new Fala("Pintas",
-                        "Muito obrigado, estudante aleatório."));
+                falas.add(new Fala(
+                        "Pintas",
+                        "Muito obrigado, estudante aleatório."
+                ));
 
-                falas.add(new Fala("Você",
-                        "Espera. Como você veio parar aqui?"));
+                falas.add(new Fala(
+                        "Você",
+                        "Espera. Como você veio parar aqui?"
+                ));
 
-                falas.add(new Fala("Pintas",
-                        "Eu sempre soube que o Capi tinha inveja de mim."));
+                falas.add(new Fala(
+                        "Pintas",
+                        "Eu sempre soube que o Capi tinha inveja de mim."
+                ));
 
-                falas.add(new Fala("Pintas",
-                        "Eu estava tomando café na DIRER quando esse maluco me sequestrou."));
+                falas.add(new Fala(
+                        "Pintas",
+                        "Eu estava tomando café na DIRER quando esse maluco me sequestrou."
+                ));
 
-                falas.add(new Fala("Você",
-                        "Espero que tudo isso conte como horas extracurriculares."));
+                falas.add(new Fala(
+                        "Você",
+                        "Espero que tudo isso conte como horas extracurriculares."
+                ));
 
-                falas.add(new Fala("Sistema",
-                        "[FIM DE JOGO]"));
+                falas.add(new Fala(
+                        "",
+                        "[FIM DE JOGO]"
+                ));
 
                 break;
         }
@@ -244,7 +341,7 @@ public class sistemaDialogo {
 
     public String getOpcao1() {
 
-        switch (localAtual) {
+        switch(localAtual) {
 
             case ENTRADA:
                 return "Ir para Bloco A";
@@ -263,7 +360,7 @@ public class sistemaDialogo {
 
     public String getOpcao2() {
 
-        switch (localAtual) {
+        switch(localAtual) {
 
             case ENTRADA:
                 return "Ir para Bloco B";
@@ -278,7 +375,7 @@ public class sistemaDialogo {
 
     public void escolha1() {
 
-        switch (localAtual) {
+        switch(localAtual) {
 
             case ENTRADA:
                 mudarLocal(Local.BLOCO_A);
@@ -297,7 +394,7 @@ public class sistemaDialogo {
 
     public void escolha2() {
 
-        switch (localAtual) {
+        switch(localAtual) {
 
             case ENTRADA:
                 mudarLocal(Local.BLOCO_B);
